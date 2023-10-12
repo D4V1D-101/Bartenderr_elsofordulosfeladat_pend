888fb69a31fa3f9fa2363a00488bcd2a7e3eb0e9
Dyan-x <kornel@bobak.ws>
2023-10-12 15:05:11 +0200

    PlayGame2 Back2
    functions

--- Assets/_scripts/switcher.cs
+++ Assets/_scripts/switcher.cs
@@ -6,15 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class switcher : MonoBehaviour
 {
+    //Take the user forwards one scene
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     
     }
+    //Takes the user forwards two scenes
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+
+    //Take the user back one scene
     public void Back()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     
+    }
+    //Take the user back two scenes
+    public void Back2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
     }
 
 }
